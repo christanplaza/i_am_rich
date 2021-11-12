edfd17cc37a5132d5d54f3adb80896c738880671
Christan Shane Plaza <christan.shane.plaza@sun-asterisk.com>
2021-11-12 17:25:47 +0800

    Initial Commit

---
+++ android/app/src/main/kotlin/christanshaneplaza/com/i_am_rich/MainActivity.kt
@@ -0,0 +1,6 @@
+package christanshaneplaza.com.i_am_rich
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+// The starting point of the app for all of our Flutter apps
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Center(
+        child: Text("Hello World"),
+      ),
+    ),
+  );
+}
